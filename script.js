fcba1f78288ee6372ebe576b3782e206f2a94d42
icwmahin <icwmahin@gmail.com>
2024-11-09 03:16:45 +0600

    first website

---
+++ script.js
@@ -0,0 +1,8 @@
+
+  const menuIcon = document.querySelector(".menu-icon");
+  const navbar = document.getElementById("navbar");
+
+  menuIcon.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+  });
+
